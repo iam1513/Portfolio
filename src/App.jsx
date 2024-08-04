b367dc7f17f13a61cf55aa1fe3df6448f1de9ec2
iam1513 <126111227+iam1513@users.noreply.github.com>
2024-08-05 00:38:38 +0530

    Added Image and changes gradient for mob view.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
 import Contact from "./components/Contact.jsx";
-<<<<<<< HEAD
-=======
-import EmailForm from "./components/EmailForm.jsx";
->>>>>>> ebe8de32c392dac6acf4a36bc07ad7bff14ffc37
 import Header from "./components/Header.jsx";
 import Home from "./components/Home.jsx";
 import Skills from "./components/Skills.jsx";
